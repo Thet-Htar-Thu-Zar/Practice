7deb68886591fb5831f6202fc8b497c42bc1a5d3
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-06-25 21:25:50 +0630

    Add project files.

---
+++ Middleware/Controllers/LoggingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Controller
+{
+  
+    public class LoggingController : ControllerBase
+    {
+        [Route("api/Log")]
+        [HttpGet]
+        public async Task<IActionResult> GetLog()
+        {
+            return Ok("Hello");
+        }
+    }
+}
